fe2ecd9e9a936fe0654d7ee45fcd15483c7bd652
ArvindTD <77227092+ArvindTD@users.noreply.github.com>
2022-08-13 21:55:29 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,18 @@
+let ownResume = {
+    Name: "Arvind",
+    Email: "tdarvind@gmail.com",
+    Phone: 9952320092,
+    Age:26,
+    Native:"Vellore",
+    Company:"Sutherland",
+    Salary: 20000,
+    Experience: "5 years",
+    Job_location:"Chennai",
+    Education: "Bachelor of Engineering",
+    Skills: ["Customer service", "Customer experience", "Service delivery", "Team managing"],
+    References: {Ramesh: 9894123456, Suresh:9952123456, Dinesh:9043123456 }
+
+}
+
+console.log(ownResume);
+
